1a15bab8ccc131355b40dafc62ccaa91151b46f4
Nick Amin <amin.nj@gmail.com>
2015-07-09 15:55:07 -0700

    bro

--- run.C
+++ run.C
@@ -1,12 +1,13 @@
 {
     gROOT->ProcessLine(".L ../software/dataMCplotMaker/dataMCplotMaker.cc+");
+    gSystem->Load("../CORE/CMS3_CORE.so");
     gROOT->ProcessLine(".L SS.cc+");
     gROOT->ProcessLine(".L scan.C+");
     //    njetsLow, njetsHigh,  btagCut, metLow, metHigh, htLow, htHigh, tag
     
-    // scan(    0    ,   9999   ,   9999  ,   0   ,   9999 ,   0  ,  9999 , ""  );
+    scan(    0    ,   9999   ,   9999  ,   0   ,   9999 ,   0  ,  9999 , ""  );
     // scan(    0    ,   9999   ,   1  ,  30   ,   9999 ,   0  ,  9999 , ""  );
 
-    scan(    0    ,   9999   ,   9999  ,  0   ,   9999 ,   0  ,  9999 , "_nocuts"  );
-    scan(    0    ,   9999   ,   1  ,  30   ,   9999 ,   0  ,  9999 , "_bt1met30"  );
+    // scan(    0    ,   9999   ,   9999  ,  0   ,   9999 ,   0  ,  9999 , "_nocuts"  );
+    // scan(    0    ,   9999   ,   1  ,  30   ,   9999 ,   0  ,  9999 , "_bt1met30"  );
 }
--- scan.C
+++ scan.C
@@ -22,28 +22,19 @@ using namespace std;
 int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999, int metLow=0, int metHigh=9999, int htLow=0, int htHigh=9999, std::string tag=""){
     //njetsLow, njetsHigh, btagCut, metLow, metHigh, htLow, htHigh, tag, manualScale
 
-    float zmassCut = 15; // line 212 of AN
+    float zmassCut = 15;
 
     TChain *ch = new TChain("t");
     std::vector<std::string> titles;
 
-
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.26/TTBAR_0.root");   titles.push_back("tt");
-
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTBAR_0.root");   titles.push_back("tt");
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/DY1_0.root");     titles.push_back("DY");
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTW_0.root");     titles.push_back("ttW");
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTZ_0.root");     titles.push_back("ttZ");
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/WJets1_0.root");  titles.push_back("WJets");
-    // ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/WZ_0.root");      titles.push_back("WZ");
-
     ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTBAR_0.root");   titles.push_back("tt");
-    ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/DY1_0.root");     titles.push_back("DY");
+    ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/DY*_0.root");     titles.push_back("DY");
     ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTW_0.root");     titles.push_back("ttW");
     ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/TTZ_0.root");     titles.push_back("ttZ");
-    ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/WJets1_0.root");  titles.push_back("WJets");
+    ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/WJets*_0.root");  titles.push_back("WJets");
     ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/WZ_0.root");      titles.push_back("WZ");
-
+    ch->Add("/nfs-7/userdata/ss2015/ssBabies/v1.27/ZZ_0.root");      titles.push_back("ZZ");
+    
     int nEventsTotal = 0;
     int nEventsChain = ch->GetEntries();
     int nGoodEvents = 0;
@@ -64,7 +55,43 @@ int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999,
     vector<TH1F*> h1D_btagval_vec;
     vector<TH1F*> h1D_mtmin_vec;
 
-    TString prevFilename = "";
+    vector<TH1F*> h1D_yields_HH_vec;
+    vector<TH1F*> h1D_yields_HL_vec;
+    vector<TH1F*> h1D_yields_LL_vec;
+
+    vector<TString> files = {"TTBAR","DY","TTW","TTZ","WJets","WZ","ZZ"}; 
+    for(int i = 0; i < files.size(); i++) {
+
+        TH1F* h1D_njets_file = new TH1F("njets"+files.at(i), "Njets;;Entries", 15, 0, 15);
+        TH1F* h1D_ht_file = new TH1F("ht"+files.at(i), "H_{T};GeV;Entries", 20, 0, 1000); 
+        TH1F* h1D_met_file = new TH1F("met"+files.at(i), "#slash{E}_{T};GeV;Entries", 20, 0, 300); 
+        TH1F* h1D_mt_file = new TH1F("mt"+files.at(i), "M_{T};GeV;Entries", 20, 0, 450); 
+        TH1F* h1D_zmass_file = new TH1F("zmass"+files.at(i), "Z Mass;GeV;Entries", 42, 70, 112); 
+        TH1F* h1D_hyp_class_file = new TH1F("hypclass"+files.at(i), "hyp_class;ID;Entries", 7, 0, 7); 
+        TH1F* h1D_nbtags_file = new TH1F("nbtags"+files.at(i), "Nbtags;nbtags;Entries", 7, 0, 7); 
+        TH1F* h1D_btagval_file = new TH1F("btagval"+files.at(i), "Btag;Btag;Entries", 30, 0, 1.0); 
+        TH1F* h1D_mtmin_file = new TH1F("mtmin"+files.at(i), "mtmin;mtmin;Entries", 20, 0, 400); 
+
+        TH1F* h1D_yields_HH_file = new TH1F("h1D_yields_HH"+files.at(i), "", 32, 1, 33); 
+        TH1F* h1D_yields_HL_file = new TH1F("h1D_yields_HL"+files.at(i), "", 26, 1, 27); 
+        TH1F* h1D_yields_LL_file = new TH1F("h1D_yields_LL"+files.at(i), "", 8, 1, 9); 
+
+        h1D_njets_vec.push_back(h1D_njets_file); 
+        h1D_ht_vec.push_back(h1D_ht_file); 
+        h1D_met_vec.push_back(h1D_met_file); 
+        h1D_mt_vec.push_back(h1D_mt_file); 
+        h1D_mtmin_vec.push_back(h1D_mtmin_file); 
+        h1D_zmass_vec.push_back(h1D_zmass_file); 
+        h1D_hyp_class_vec.push_back(h1D_hyp_class_file); 
+        h1D_nbtags_vec.push_back(h1D_nbtags_file); 
+        h1D_btagval_vec.push_back(h1D_btagval_file); 
+
+        h1D_yields_HH_vec.push_back(h1D_yields_HH_file);
+        h1D_yields_HL_vec.push_back(h1D_yields_HL_file);
+        h1D_yields_LL_vec.push_back(h1D_yields_LL_file);
+
+    }
+
     initCounter();
     // File Loop
     while ( (currentFile = (TFile*)fileIter.Next()) ) { 
@@ -75,45 +102,17 @@ int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999,
         samesign.Init(tree);
 
         TString filename(currentFile->GetTitle());
-        if(filename != prevFilename) {
-            std::cout << filename << std::endl;
-            prevFilename = filename;
-        }
 
-        if(filename.Contains("TTBAR")) filename = "TTBAR";
-        else if(filename.Contains("DY")) filename = "DY";
-        else if(filename.Contains("TTW")) filename = "TTW";
-        else if(filename.Contains("TTZ")) filename = "TTZ";
-        else if(filename.Contains("WJets")) filename = "WJets";
-        else if(filename.Contains("WZ")) filename = "WZ";
-        else {
-            std::cout << "I don't know what " << filename << " is!" << std::endl;
-            std::cout << "I don't know what " << filename << " is!" << std::endl;
-        }
+        int iSample = -1;
 
-
-        // TObjArray *tx = filename.Tokenize("_");
-        // filename = ((TObjString *)(tx->At(0)))->String();
-
-        TH1F* h1D_njets_file = new TH1F("njets"+filename, "Njets;;Entries", 15, 0, 15); h1D_njets_file->Sumw2();
-        TH1F* h1D_ht_file = new TH1F("ht"+filename, "H_{T};GeV;Entries", 20, 0, 600); 
-        TH1F* h1D_met_file = new TH1F("met"+filename, "#slash{E}_{T};GeV;Entries", 20, 0, 300); 
-        TH1F* h1D_mt_file = new TH1F("mt"+filename, "M_{T};GeV;Entries", 20, 0, 400); 
-        TH1F* h1D_zmass_file = new TH1F("zmass"+filename, "Z Mass;GeV;Entries", 42, 70, 112); 
-        TH1F* h1D_hyp_class_file = new TH1F("hypclass"+filename, "hyp_class;ID;Entries", 7, 0, 7); 
-        TH1F* h1D_nbtags_file = new TH1F("nbtags"+filename, "Nbtags;nbtags;Entries", 7, 0, 7); 
-        TH1F* h1D_nbtagval_file = new TH1F("btagval"+filename, "Btag;Btag;Entries", 30, 0, 1.0); 
-        TH1F* h1D_mtmin_file = new TH1F("mtmin"+filename, "mtmin;mtmin;Entries", 20, 0, 400); 
-
-        h1D_njets_vec.push_back(h1D_njets_file); 
-        h1D_ht_vec.push_back(h1D_ht_file); 
-        h1D_met_vec.push_back(h1D_met_file); 
-        h1D_mt_vec.push_back(h1D_mt_file); 
-        h1D_mtmin_vec.push_back(h1D_mtmin_file); 
-        h1D_zmass_vec.push_back(h1D_zmass_file); 
-        h1D_hyp_class_vec.push_back(h1D_hyp_class_file); 
-        h1D_nbtags_vec.push_back(h1D_nbtags_file); 
-        h1D_btagval_vec.push_back(h1D_nbtagval_file); 
+        if(filename.Contains("TTBAR"))      { filename = "TTBAR"; iSample = 0; }
+        else if(filename.Contains("DY"))    { filename = "DY";    iSample = 1; }
+        else if(filename.Contains("TTW"))   { filename = "TTW";   iSample = 2; }
+        else if(filename.Contains("TTZ"))   { filename = "TTZ";   iSample = 3; }
+        else if(filename.Contains("WJets")) { filename = "WJets"; iSample = 4; }
+        else if(filename.Contains("WZ"))    { filename = "WZ";    iSample = 5; }
+        else if(filename.Contains("ZZ"))    { filename = "ZZ";    iSample = 6; }
+        else { std::cout << "ERROR: I don't know what " << filename << " is! Skipping file " << filename << std::endl; continue; }
 
         // Loop over Events in current file
         for( unsigned int event = 0; event < tree->GetEntriesFast(); ++event) {
@@ -128,12 +127,10 @@ int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999,
             float scale = ss::scale1fb()  * luminosity;
 
             // fill these before making cuts
-            fill(h1D_hyp_class_file, ss::hyp_class(), scale);
+            fill(h1D_hyp_class_vec.at(iSample), ss::hyp_class(), scale);
 
             // this guarantees that the third lepton makes a Z with one of the first two leptons
             if(ss::hyp_class() != 6) continue;
-
-
 
             // require that leptons have pt>20 and |eta|<2.4 ("loose requirements")
             if( ss::lep1_p4().pt() < 20.0 || ss::lep2_p4().pt() < 20.0 || ss::lep3_p4().pt() < 20.0 ) continue;
@@ -169,28 +166,36 @@ int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999,
             if(fabs(zmass - 91.2) > zmassCut) continue;
             if(ss::ht() < htLow || ss::ht() > htHigh) continue;
             if(ss::njets() < njetsLow || ss::njets() > njetsHigh) continue;
-
-            // We are now in the region of interest
-
-
-            fill(h1D_nbtags_file, ss::nbtags(), scale);
             if(ss::nbtags() >= btagCut) continue;
 
-            for(int iJet = 0; iJet < ss::jets().size(); iJet++) {
-                fill(h1D_nbtagval_file, ss::jets_disc().at(iJet),scale);
+            // We are now in the region of interest
+            anal_type_t categ = analysisCategory(ss::lep1_p4().pt(), ss::lep2_p4().pt());  
+            int SR = signalRegion(ss::njets(), ss::nbtags(), ss::met(), ss::ht(), ss::mtmin(), ss::lep1_p4().pt(), ss::lep2_p4().pt());
+            int BR = baselineRegion(ss::njets(), ss::nbtags(), ss::met(), ss::ht(), ss::lep1_p4().pt(), ss::lep2_p4().pt());
+
+            // Select certain SRs. XXX Remember to update spec below too.
+            if(SR < 1 || SR > 8 || categ != HighHigh) continue;
+
+            if(SR > -1) {
+                if(categ == HighHigh) h1D_yields_HH_vec.at(iSample)->Fill(SR, scale);
+                else if(categ == HighLow) h1D_yields_HL_vec.at(iSample)->Fill(SR, scale);
+                else if(categ == LowLow) h1D_yields_LL_vec.at(iSample)->Fill(SR, scale);
             }
-            fill(h1D_mt_file,ss::mt(), scale);
-            fill(h1D_mtmin_file,ss::mtmin(), scale);
-            fill(h1D_zmass_file,zmass, scale); 
-            fill(h1D_njets_file,ss::njets(), scale);
-            fill(h1D_ht_file,ss::ht(), scale);
-            fill(h1D_met_file,ss::met(), scale);
+
+            for(int iJet = 0; iJet < ss::jets().size(); iJet++) {
+                fill(h1D_btagval_vec.at(iSample), ss::jets_disc().at(iJet),scale);
+            }
+            fill(h1D_nbtags_vec.at(iSample), ss::nbtags(), scale);
+            fill(h1D_mt_vec.at(iSample),ss::mt(), scale);
+            fill(h1D_mtmin_vec.at(iSample),ss::mtmin(), scale);
+            fill(h1D_zmass_vec.at(iSample),zmass, scale); 
+            fill(h1D_njets_vec.at(iSample),ss::njets(), scale);
+            fill(h1D_ht_vec.at(iSample),ss::ht(), scale);
+            fill(h1D_met_vec.at(iSample),ss::met(), scale);
 
             nGoodEvents++;
             nGoodEventsWeighted+=scale;
             addToCounter(filename, scale);
-
-
 
         }//event loop
 
@@ -200,18 +205,26 @@ int scan(unsigned int njetsLow=0, unsigned int njetsHigh=9999, int btagCut=9999,
     printCounter();
 
     TH1F* null = new TH1F("","",1,0,1);
-    // std::string com = "--preserveBackgroundOrder --showPercentage --outputName pdfs"+tag+"/";
-    std::string com = "--noDivisionLabel --showPercentage --outputName pdfs"+tag+"/";
+    std::string com = "--noDivisionLabel --outputName pdfs"+tag+"/";
+    std::string pct = " --showPercentage ";
+    std::string spec = "SR1-8";
 
-    dataMCplotMaker(null,h1D_njets_vec     ,titles,"Njets","",com+"h1D_njets.pdf                   --isLinear --xAxisOverride njets ");
-    dataMCplotMaker(null,h1D_ht_vec        ,titles,"H_{T}","",com+"h1D_ht.pdf                      --isLinear --xAxisOverride [GeV] ");
-    dataMCplotMaker(null,h1D_mt_vec        ,titles,"m_{T}","",com+"h1D_mt.pdf                      --isLinear --xAxisOverride [GeV] ");
-    dataMCplotMaker(null,h1D_mtmin_vec     ,titles,"m_{T,min}","",com+"h1D_mtmin.pdf               --isLinear --xAxisOverride [GeV] ");
-    dataMCplotMaker(null,h1D_met_vec       ,titles,"#slash{E}_{T}","",com+"h1D_met.pdf             --isLinear --xAxisOverride [GeV] ");
-    dataMCplotMaker(null,h1D_zmass_vec     ,titles,"m_{Z}","",com+"h1D_zmass.pdf                   --isLinear --xAxisOverride [GeV] ");
-    dataMCplotMaker(null,h1D_hyp_class_vec ,titles,"hyp_class (no cuts)","",com+"h1D_hyp_class.pdf            --xAxisOverride id    ");
-    dataMCplotMaker(null,h1D_nbtags_vec    ,titles,"Nbtags (no cuts)","",com+"h1D_nbtags.pdf                  --xAxisOverride n     ");
-    dataMCplotMaker(null,h1D_btagval_vec   ,titles,"Btag disc","",com+"h1D_btagval.pdf             --isLinear --xAxisOverride disc  ");
+    dataMCplotMaker(null,h1D_njets_vec     ,titles,"Njets",spec,com+"h1D_njets.pdf                   --isLinear --xAxisOverride njets "+pct);
+    dataMCplotMaker(null,h1D_ht_vec        ,titles,"H_{T}",spec,com+"h1D_ht.pdf                      --isLinear --xAxisOverride [GeV] "+pct);
+    dataMCplotMaker(null,h1D_mt_vec        ,titles,"m_{T}",spec,com+"h1D_mt.pdf                      --isLinear --xAxisOverride [GeV] "+pct);
+    dataMCplotMaker(null,h1D_mtmin_vec     ,titles,"m_{T,min}",spec,com+"h1D_mtmin.pdf               --isLinear --xAxisOverride [GeV] "+pct);
+    dataMCplotMaker(null,h1D_met_vec       ,titles,"#slash{E}_{T}",spec,com+"h1D_met.pdf             --isLinear --xAxisOverride [GeV] "+pct);
+    // dataMCplotMaker(null,h1D_zmass_vec     ,titles,"m_{Z}",spec,com+"h1D_zmass.pdf                   --isLinear --xAxisOverride [GeV] "+pct);
+    // dataMCplotMaker(null,h1D_hyp_class_vec ,titles,"hyp_class (no cuts)",spec,com+"h1D_hyp_class.pdf            --xAxisOverride id    "+pct);
+    dataMCplotMaker(null,h1D_nbtags_vec    ,titles,"Nbtags",spec,com+"h1D_nbtags.pdf                  --xAxisOverride n     "+pct);
+    // dataMCplotMaker(null,h1D_btagval_vec   ,titles,"Btag disc",spec,com+"h1D_btagval.pdf             --isLinear --xAxisOverride disc  "+pct);
+
+    std::string HHbins = " --xAxisVerticalBinLabels --xAxisBinLabels 1A,2A,3A,4A,5A,6A,7A,8A,9A,10A,11A,12A,13A,14A,15A,16A,17A,18A,19A,20A,21A,22A,23A,24A,25A,26A,27A,28A,29A,30A,31A,32A";
+    std::string HLbins = " --xAxisVerticalBinLabels --xAxisBinLabels 1B,2B,3B,4B,5B,6B,7B,8B,9B,10B,11B,12B,13B,14B,15B,16B,17B,18B,19B,20B,21B,22B,23B,24B,25B,26B";
+    std::string LLbins = " --xAxisVerticalBinLabels --xAxisBinLabels 1C,2C,3C,4C,5C,6C,7C,8C";
+    dataMCplotMaker(null,h1D_yields_HH_vec ,titles,"HH yields",spec,com+"h1D_yields_HH.pdf --isLinear --xAxisOverride SR "+HHbins+pct);
+    dataMCplotMaker(null,h1D_yields_HL_vec ,titles,"HL yields",spec,com+"h1D_yields_HL.pdf --isLinear --xAxisOverride SR "+HLbins+pct);
+    dataMCplotMaker(null,h1D_yields_LL_vec ,titles,"LL yields",spec,com+"h1D_yields_LL.pdf --isLinear --xAxisOverride SR "+LLbins+pct);
 
     return 0;
 }
